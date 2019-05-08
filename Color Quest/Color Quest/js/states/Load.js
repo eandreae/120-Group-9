37a9439b738c46644aad3284c62f1c1677fdfe0a
Eric Hu <eric.jie.hu@gmail.com>
2019-05-07 23:09:21 -0700

    Update Load.js
    Loading assets

--- Color Quest/Color Quest/js/states/Load.js
+++ Color Quest/Color Quest/js/states/Load.js
@@ -2,15 +2,19 @@
 var Load = function(game) {};
 Load.prototype = {
 	preload: function() {
-
+		game.load.path = 'assets/img/';
+		game.load.image('ground', 'platform.png');
+		game.load.image('player', 'diamond.png');
+		game.load.image('bullet', 'bullet.png');
 	},
 	create: function() {
+		console.log('Load: create');
 		// check for local storage browser support
 
 	},
 
 	update: function() {
-      console.log('Load: update');
+      //console.log('Load: update');
       // GameOver logic
       if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
           game.state.start('MainMenu');
