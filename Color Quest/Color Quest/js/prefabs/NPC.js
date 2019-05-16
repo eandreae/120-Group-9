0c8795e6bc01554a0f7a8d0007947ba0bc7d493c
Eric Hu <eric.jie.hu@gmail.com>
2019-05-15 22:39:33 -0700

    Starting NPC Prefab

---
+++ Color Quest/Color Quest/js/prefabs/NPC.js
@@ -0,0 +1,34 @@
+this.v = 0;
+function NPC(game, x, y, speed) {
+   this.v = speed;
+   //console.log(velocity);
+   // Inherits Phaser.Sprite and puts the sprite in a position on the screen
+   Phaser.Sprite.call(this, game, x, y, 'atlas', 'badguy01');
+
+   game.physics.arcade.enable(this);
+   this.body.collideWorldBounds = true;
+   this.body.velocity.x = this.v;
+   //this.body.immovable = true;
+   this.body.gravity.y = 300;
+
+   // add the animations for the enemies.
+   this.animations.add('right', ['badguy01', 'badguy02'], 10, true);
+   this.animations.add('left', ['badguy03', 'badguy04'], 10, true);
+}
+
+// Define NPC's prototype and constructor
+NPC.prototype = Object.create(Phaser.Sprite.prototype);
+NPC.prototype.constructor = NPC;
+
+NPC.prototype.update = function() {
+   this.body.velocity.x = this.v;
+   // if moving to the right.
+   if( this.body.velocity.x > 0 ){
+       this.animations.play('right');
+   }
+   // if moving to the right.
+   if( this.body.velocity.x < 0 ){
+       this.animations.play('left');
+   }
+
+}
