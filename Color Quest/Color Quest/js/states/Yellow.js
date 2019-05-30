6910254065cb35bd3722b9d65e99ce4cf9958b1d
Eric Hu <eric.jie.hu@gmail.com>
2019-05-29 22:00:34 -0700

    Commenting Yellow.js
    Commenting and updating the code in Yellow.js

--- Color Quest/Color Quest/js/states/Yellow.js
+++ Color Quest/Color Quest/js/states/Yellow.js
@@ -2,21 +2,13 @@
 var Yellow = function(game) {};
 Yellow.prototype = {
 
-   // Variables used in Yellow
-   init: function() {
-
-   },
-
+   // Preload the tilemap
    preload: function() {
-      console.log('Yellow: preload');
-
       game.load.tilemap('layout', 'assets/TileMaps/Yellow.json', null, Phaser.Tilemap.TILED_JSON);
       game.load.spritesheet('tilesheet', 'assets/TileMaps/color_tiles.png', 32, 32);
    },
 
    create: function() {
-      console.log('Yellow: create');
-
       // Music
       song.stop();
       song = game.add.audio('action2');
@@ -33,17 +25,16 @@ Yellow.prototype = {
       this.map = game.add.tilemap('layout');
       this.map.addTilesetImage('color_tiles_tileset', 'tilesheet');
 
-      if( yellowLevel == 0 ){
-          this.mapLayer = this.map.createLayer('Ground_0');
-          this.map.setCollisionBetween(0, 999, true, 'Ground_0');
-      }
-      else if ( yellowLevel == 1 ){
-          this.mapLayer = this.map.createLayer('Ground_1');
-          this.map.setCollisionBetween(0, 999, true, 'Ground_1');
-      }
-      else if ( yellowLevel == 2 ){
-          this.mapLayer = this.map.createLayer('Ground_2');
-          this.map.setCollisionBetween(0, 999, true, 'Ground_2');
+      // The layers for each yellow level
+      if (yellowLevel == 0) {
+         this.mapLayer = this.map.createLayer('Ground_0');
+         this.map.setCollisionBetween(0, 999, true, 'Ground_0');
+      } else if (yellowLevel == 1) {
+         this.mapLayer = this.map.createLayer('Ground_1');
+         this.map.setCollisionBetween(0, 999, true, 'Ground_1');
+      } else if (yellowLevel == 2) {
+         this.mapLayer = this.map.createLayer('Ground_2');
+         this.map.setCollisionBetween(0, 999, true, 'Ground_2');
       }
 
       this.mapLayer.resizeWorld();
@@ -73,11 +64,12 @@ Yellow.prototype = {
       // game.add.existing(enemy);
       // this.enemies.add(enemy);
 
-      // Red square bit map data
+      // Invisible collision to trigger the wall
       bmd = game.add.bitmapData(10, 10000);
       this.x = game.add.sprite(1350, 0, bmd);
       game.physics.arcade.enable(this.x);
 
+      // Wall of death that chases the player when activated
       bmd = game.add.bitmapData(10000, 1000);
       bmd.fill(127, 106, 0, 1);
       this.wall = game.add.sprite(-10100, 200, bmd);
@@ -90,8 +82,6 @@ Yellow.prototype = {
    },
 
    update: function() {
-      //console.log('Yellow: update');
-
       // Making it so the enemy will follow the Player.
       // If player is on the enemy's left.
       // if( this.enemy.body.x < this.player.body.x ){
@@ -116,60 +106,63 @@ Yellow.prototype = {
          this.playerBullets.add(bullet);
       }
 
-      this.physics.arcade.overlap(this.player, this.yellow, collectYellow, null, this);
+      this.physics.arcade.overlap(this.player, this.yellow, this.collectYellow, null, this);
+      this.physics.arcade.overlap(this.player, this.x, this.startWall, null, this);
 
-      this.physics.arcade.overlap(this.player, this.x, startWall, null, this);
-
+      // If the player touches the death wall, they instantly die
       if (this.physics.arcade.collide(this.player, this.wall)) {
+         health = 0;
          playerDies(this, this.player, 'Yellow');
       }
+   },
 
-      // When the player collects the color
-      function collectYellow(player, color) {
-          // increment the yellowLevel progress.
-          yellowLevel++;
-          if( yellowLevel == 3 ){
-              hasYellow = true;
-          }
-
-         // Yellow bdm
-         bmd = game.add.bitmapData(18, 18);
-         bmd.fill(255, 255, 0, 1);
-
-         // Particles when color is collected
-         colorEmitter = game.add.emitter(color.x, color.y, 200);
-         colorEmitter.makeParticles(bmd);		        // red squares used as particles
-         colorEmitter.gravity = 0;
-         colorEmitter.setScale(.25, .8, .25, .8, 0);
-         colorEmitter.setAlpha(.8, 0, 1800); 	      // .8 to .3 alpha
-         colorEmitter.setXSpeed(-100,100);			   // horizontal speed range
-         colorEmitter.setYSpeed(-100,100);			   // vertical speed range
-         colorEmitter.start(true, 2000, null, 50);	   // (explode, lifespan, freq, quantity)
-
-         color.destroy();
-         song.stop();
-         this.wall.body.velocity.x = 0;
-         game.time.events.add(Phaser.Timer.SECOND * 2, function() {
-             if( yellowLevel == 3 ){
-                 game.state.start('Tutorial')
-             } else {
-                 game.state.start('Yellow')
-             }
-
-         });
+   // When the player collects the color
+   collectYellow: function(player, color) {
+      // increment the yellowLevel progress.
+      yellowLevel++;
+      if (yellowLevel == 3) {
+         hasYellow = true;
       }
 
-      function startWall(player, x) {
-          song.stop();
-          song = game.add.audio('action');
-          song.play('', 0, 0.5, true);
-         x.destroy();
-         this.wall.body.velocity.x = 300;
-      }
-  },
-  render: function() {
-     game.debug.bodyInfo(this.player, 100, 100, 'black');
-     game.debug.body(this.player);
-     game.debug.body(this.x);
-  }
+      // Yellow bdm
+      bmd = game.add.bitmapData(18, 18);
+      bmd.fill(255, 255, 0, 1);
+
+      // Particles when color is collected
+      colorEmitter = game.add.emitter(color.x, color.y, 200);
+      colorEmitter.makeParticles(bmd); // red squares used as particles
+      colorEmitter.gravity = 0;
+      colorEmitter.setScale(.25, .8, .25, .8, 0);
+      colorEmitter.setAlpha(.8, 0, 1800); // .8 to .3 alpha
+      colorEmitter.setXSpeed(-100, 100); // horizontal speed range
+      colorEmitter.setYSpeed(-100, 100); // vertical speed range
+      colorEmitter.start(true, 2000, null, 50); // (explode, lifespan, freq, quantity)
+
+      color.destroy();
+      song.stop();
+      this.wall.body.velocity.x = 0;
+      game.time.events.add(Phaser.Timer.SECOND * 2, function() {
+         if (yellowLevel == 3) {
+            game.state.start('Tutorial')
+         } else {
+            game.state.start('Yellow')
+         }
+      });
+   },
+
+   // Starts the death wall
+   startWall: function(player, x) {
+      song.stop();
+      song = game.add.audio('action');
+      song.play('', 0, 0.5, true);
+      x.destroy();
+      this.wall.body.velocity.x = 300;
+   },
+
+   // Debug stuff
+   render: function() {
+      game.debug.bodyInfo(this.player, 100, 100, 'black');
+      game.debug.body(this.player);
+      game.debug.body(this.x);
+   }
 };
